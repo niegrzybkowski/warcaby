224ebca78161adaa59322c206db6424113523313
karik16 <karol.degorski@onet.eu>
2021-11-21 16:55:11 +0100

    Update page1.js

--- frontend/page1.js
+++ frontend/page1.js
@@ -14,7 +14,6 @@ color_board = function(){
         }
         let firstchar = Number(j.charAt(0));
         let secondchar = Number(j.charAt(1));
-        console.log(firstchar+secondchar);
         let field = document.getElementById(j);
         if((firstchar+secondchar)%2==1){
             field.style.backgroundColor = "brown";
@@ -28,34 +27,33 @@ color_board = function(){
 }
 
 initialize_pawns = function(){
-    for(let i = 1; i < 40; i = i + 2){
+    for(let i = 1; i < 40; i = i + 1){
         let j;
+
         if(i<10){
             j = '0' + i;
         }
-        if(i>=10){
-            if(i==11 || i==31){
-                i--;
-            }
+        else{
             j = i.toString();
         }
+        let firstchar = Number(j.charAt(0));
+        let secondchar = Number(j.charAt(1));
         let pawn = document.getElementById(j);
-        pawn.style.backgroundColor = "black";
-        if(i==18){
-            i++;
+        if((firstchar+secondchar)%2==1){
+            pawn.style.backgroundColor = "black";
         }
+
     }
-    for(let i = 98; i >= 60; i = i - 2){
+    for(let i = 99; i >= 60; i = i - 1){
         let j;
-        if(i==88 || i==68){
-            i++;
-        }
-        if(i==79){
-            i--;
-        }
         j = i.toString();
+        let firstchar = Number(j.charAt(0));
+        let secondchar = Number(j.charAt(1));
         let pawn = document.getElementById(j);
-        pawn.style.backgroundColor = "yellow";
+        if((firstchar+secondchar)%2==1){
+            pawn.style.backgroundColor = "yellow";
+        }
+
         
     }
     
