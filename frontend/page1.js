aa85ae1fd682499e18038c7967b78f0edaceb55a
karik16 <karol.degorski@onet.eu>
2021-11-21 16:51:10 +0100

    add colors to chessboard

--- frontend/page1.js
+++ frontend/page1.js
@@ -1,5 +1,30 @@
 window.onload = function (){
     initialize_pawns();
+    color_board()
+}
+
+color_board = function(){
+    for(let i = 0; i < 100; i ++){
+        let j;
+        if(i<10){
+            j = '0' + i + 'td';
+        }
+        else{
+            j = i + 'td';
+        }
+        let firstchar = Number(j.charAt(0));
+        let secondchar = Number(j.charAt(1));
+        console.log(firstchar+secondchar);
+        let field = document.getElementById(j);
+        if((firstchar+secondchar)%2==1){
+            field.style.backgroundColor = "brown";
+        }
+        else{
+            field.style.backgroundColor = "#f5b342";
+        }
+        
+
+    }
 }
 
 initialize_pawns = function(){
