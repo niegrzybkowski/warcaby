bdcf69bac647893fff283884c094c663d9b5985b
karik16 <karol.degorski@onet.eu>
2021-12-20 20:20:38 +0100

    add generate html

--- home/views.py
+++ home/views.py
@@ -4,19 +4,22 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-
+from . import config
 
 
 def home(req: HttpRequest):
     if req.method == 'GET' and "name" in req.GET.keys():
         return HttpResponse(f"Hello, {req.GET['name']}")
     else:
-        return HttpResponse(f"Hello, stranger!")
-
-from .models import Room
+        return HttpResponse(f"<h1>Hello, stranger!")
 
 def createRoom(player):
     room = Room.objects.create(player_id = player.player_id)
+
+def createSpace():
+    with open(f'space{config.it}.html') as f:
+        f.write('<h1>I am room no 1</h1>')
+    config.it += 1
 
 
 
