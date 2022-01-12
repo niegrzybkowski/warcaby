2d5be840837e760750bd2bdd42c611f28fb2caa4
karik16 <karol.degorski@onet.eu>
2022-01-12 19:18:20 +0100

    make rooms work

--- local/views.py
+++ local/views.py
@@ -1,6 +1,6 @@
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
