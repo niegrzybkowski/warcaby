3924359c1f2850596b335b1a1cae2059b903d50d
karik16 <karol.degorski@onet.eu>
2022-01-12 19:11:05 +0100

    update urls

--- local/urls.py
+++ local/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from local import views
+from local.views import game
+from local.views import local
 
 urlpatterns = [
     path('new/', views.new),
-    path('play/', views.local)
+    path('play/', views.local),
+    path('play/<room_code>', game),
+    path('', local),
 ]
