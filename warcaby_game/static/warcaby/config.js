964d6e653ca6d6de993220c13670fffbbc01401d
karik16 <karol.degorski@onet.eu>
2022-01-21 21:53:01 +0100

    Update config.js

--- warcaby_game/static/warcaby/config.js
+++ warcaby_game/static/warcaby/config.js
@@ -1,5 +1,8 @@
+const size = document.getElementById("size");
+
 function validateForm(e) {
     e.preventDefault();
+    if(document.getElementById("size"))
     alert("Name must be filled out");
     return false;
   }
