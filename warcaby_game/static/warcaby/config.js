0c4cbfc9716770da45c0daf5b45e0163610e6265
karik16 <karol.degorski@onet.eu>
2022-01-23 15:43:26 +0100

    add customized form check

--- warcaby_game/static/warcaby/config.js
+++ warcaby_game/static/warcaby/config.js
@@ -1,8 +1,64 @@
-const size = document.getElementById("size");
+
+$(document).ready(function() {
+  $('#size').on('input', function() {
+    var size=$(this);
+    var size_val=size.val();
+    var err1 = document.getElementById("error1");
+
+    if(size_val < 4 || Math.floor(size_val) != size_val)
+    {
+      err1.classList.remove("error");
+      err1.classList.add("show_error");
+
+    }
+    else{
+      err1.classList.remove("show_error");
+      err1.classList.add("error");    
+    }
+
+  });
+
+  $('#starting_rows').on('input', function() {
+    var starting_rows=$(this);
+    var starting_rows_val=starting_rows.val();
+    var err2 = document.getElementById("error2");
+
+    if(starting_rows_val < 2 || Math.floor(starting_rows_val) != starting_rows_val)
+    {
+      err2.classList.remove("error");
+      err2.classList.add("show_error");
+
+    }
+    else{
+      err2.classList.remove("show_error");
+      err2.classList.add("error");    
+    }
+  });
+
+
+
+});
 
 function validateForm(e) {
+  var err1 = document.getElementById("error1");
+  var err2 = document.getElementById("error2");
+  if(err1.className == "show_error" || err2.className == "show_error"){
     e.preventDefault();
-    if(document.getElementById("size"))
-    alert("Name must be filled out");
     return false;
   }
+  var size=document.getElementById("size");
+  var size_val=size.value;   
+  var starting_rows=document.getElementById("starting_rows");
+  var starting_rows_val=starting_rows.value; 
+  var err3 = document.getElementById("error3");
+  if(size_val - 2 * starting_rows_val < 2){
+    e.preventDefault();
+    err3.classList.remove("error");
+    err3.classList.add("show_error");
+    return false;
+  }
+  else{
+    err3.classList.remove("show_error");
+    err3.classList.add("error");
+  }
+}
